8e81fa6c4ee7c0456c8a574f7e5056f895a89239
Michael Pelz-Sherman <msherman@Michaels-FM-Laptop.local>
2012-02-12 11:22:04 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RewardTracker::Application.config.secret_token = '68277b884f650031b587138423618ce914691c7f2ae23d6e830787065a52e6486d8c2822efab3f3af789a4d12fb085e47f727fd868ee07ebd285b7e03661b638'
